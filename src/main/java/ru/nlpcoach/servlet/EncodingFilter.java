1dcceb7318f15ef2ca9bd4da268eb9f8602f8c0d
Pavel <triodjangopiter@yandex.ru>
2017-08-01 16:18:41 +0300

    add encoding filter to utf8

--- src/main/java/ru/nlpcoach/servlet/EncodingFilter.java
+++ src/main/java/ru/nlpcoach/servlet/EncodingFilter.java
@@ -1,13 +1,26 @@
-public class EncodingFilter implements javax.servlet.Filter {
+package ru.nlpcoach.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
     public void destroy() {
     }
-
-    public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, java.io.IOException {
-        chain.doFilter(req, resp);
-    }
-
-    public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {
-
-    }
-
 }
